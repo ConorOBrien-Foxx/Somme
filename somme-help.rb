244e5f7a7fc0cd56e36b3edb6c811c3e3c2808ed
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2018-06-29 18:21:25 -0400

    add a helper

---
+++ somme-help.rb
@@ -0,0 +1,25 @@
+dest = 'Asi:8+::2+:47*-m,'
+
+def b(s,n,m);k=0;k+=1 while (s+k)%m!=n;k;end
+def to_grid(str)
+    s =     str.split "\n"
+    maxlen = s.map(&:length).max
+    s.map { |l| l.ljust(maxlen).chars }
+end
+prog = <<EOF.chomp
+void main(){import std.stdio;"D".write;}
+/*
+*/
+EOF
+dest = dest.ljust prog.size
+class Array;def sum;inject(0,:+);end;end
+def gsum(arr)
+    arr.map { |e| e.ord - 32 } .sum
+end
+tr = to_grid(prog).transpose
+puts prog
+tr.map.with_index { |col, i|
+    a = b(gsum(col), dest[i].ord - 32, 95)
+    print (a+32).chr
+}
+print dest[tr.size..-1]
